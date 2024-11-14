60d612896091af0f04328c1ed86e5e634cd76d21
ericson949 <ericson949@gmail.com>
2024-11-14 06:49:46 +0100

    [UPDATE] add apps tabs

--- employees-manage-mobile/app/(tabs)/(companies)/index.tsx
+++ employees-manage-mobile/app/(tabs)/(companies)/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { FlashList, MasonryFlashList } from "@shopify/flash-list";
+import React from "react";
+import { View, Text, StatusBar, Dimensions } from "react-native";
+
+
+const DATA = [
+  {
+    title: "First Item",
+  },
+  {
+    title: "Second Item",
+  },
+];
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+  
+    <View style={{ height: Dimensions.get("screen").height, width: Dimensions.get("screen").width }}>
+      <MasonryFlashList
+      data={DATA}
+      numColumns={2}
+      renderItem={({ item }) => <Text>{item.title}</Text>}
+      estimatedItemSize={200}
+    />
     </View>
   );
 }
